8030d69d7ce51a9944ea1e7a635fea6daac106ea
wangyan <251140742@qq.com>
2019-07-22 18:21:28 +0800

    获取专业

--- src/components/Teachers/MyMajor.vue
+++ src/components/Teachers/MyMajor.vue
@@ -3,10 +3,11 @@
 		<div class="mahorTitle">
 			<div>
 				<div>
-					移动互联(web前端方向)
+					<!-- 移动互联(web前端方向) -->
+					{{MajorCustomData.majorTypeName}}
 				</div>
 				<ul>
-					<li>共5个学期</li>
+					<li>共8个学期</li>
 					<li>14门课程</li>
 					<li>514节课时</li>
 				</ul>
@@ -15,59 +16,16 @@
 		<el-collapse v-model="activeNames">
 			 <p class="major top"><span class="redSquare"></span><span>专业介绍</span></p>
 			 <p class="major"><span class="redSquare"></span><span>专业章节</span></p>
-			<el-collapse-item title="第一学期" name="1">
-				<div>
-					<a href="#">WEB前端基础(HTML+CSS)</a>
-				</div>
-				<div>
-					<a href="#">操作系统应用(Linux)</a>
-				</div>
-				<div>
-					<a href="#">编程语言基础(PHP)</a>
-				</div>
-			</el-collapse-item>
-			<el-collapse-item title="第二学期" name="2">
-				<div>
-					<a href="#">脚本语言基础(JavaScript)</a>
-				</div>
-				<div>
-					<a href="#">WEB前端高级(HTML5+CSS3)</a>
-				</div>
-				<div>
-					<a href="#">数据库基础(MySQL)</a>
-				</div>
-			</el-collapse-item>
-			<el-collapse-item title="第三学期" name="3">
-				<div>
-					<a href="#">服务端应用开发(PHP)</a>
-				</div>
-				<div>
-					<a href="#">脚本语言高级(jQuery+Ajax)</a>
-				</div>
-				<div>
-					<a href="#">响应式设计(Bootstrap)</a>
-				</div>
-			</el-collapse-item>
-			<el-collapse-item title="第四学期" name="4">
-				<div>
-					<a href="#">前端框架(Vue)</a>
-				</div>
-				<div>
-					<a href="#">Vue项目实战</a>
-				</div>
-				<div>
-					<a href="#">Node.js</a>
-				</div>
-			</el-collapse-item>
-			<el-collapse-item title="第五学期" name="5">
-				<div>
-					<a href="#">就业岗前冲刺</a>
-				</div>
-				<div>
-					<a href="#">全真项目实训</a>
-				</div>
-			</el-collapse-item>
-		</el-collapse>
+			 <div v-for="(item,index) in MajorCustomData.majorCustomItemTreeAdapterList" :key='index'>
+				 <!-- <span>{{item}}</span> -->
+				 <el-collapse-item :title="item.name" :name="index">
+					<div v-for='(itemMajor,index) in item.childList' :key='index'>
+						<a href="#">{{itemMajor.name}}</a>
+					</div>
+				</el-collapse-item>
+			 </div>
+		
+		</el-collapse> 
 	</div>
 </template>
 
@@ -76,13 +34,26 @@
 		name: 'myMajor',
 		data() {
 			return {
-				activeNames: ['1', '2', '3', '4', '5']
+				activeNames: ['0','1', '2', '3', '4', '5','6','7'],
+				MajorCustomData:''
 			};
 		},
 		methods: {
-			handleChange(val) {
-				console.log(val);
+			//获取该教师的专业
+			getMajorCustom(userId){
+				var app = this;
+				this.$http
+					.get(`/product/majorCustom/getMajorCustomByUser/${userId}`)
+					.then(function(res) {
+						app.MajorCustomData=res.data[0];
+						console.log(res.data[0])
+					});
 			}
+			
+		},
+		created(){
+			this.userId = window.localStorage.getItem("userId");
+			this.getMajorCustom(this.userId);
 		}
 	}
 </script>
